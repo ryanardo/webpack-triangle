1d0d1cb4b2b8011e5d9282f9ac32a28a984d5459
Adam Calhoun and Ryan Ferris <adam+ryan@>
2018-02-08 14:00:02 -0800

    Add jQuery and Bootstrap to project.

--- app/src/index.js
+++ app/src/index.js
@@ -1,40 +1,28 @@
 import * as Triangle from './../js/triangle.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-if ( process.env.NODE_ENV !== 'production' ) {
-	console.log( 'Looks like we are in the development mode!' );
-}
-console.log( "index.js successfully run" );
-
-function component() {
-	// This line creates the html div element that webpack gets to control.
-	let element = document.createElement( 'pre' );
-
-	element.innerHTML = [ 'Hello webpack!' ];
-
-	return element;
+if(process.env.NODE_ENV !== 'production') {
+	console.log('Looks like we are in the development mode!');
 }
 
-document.body.append( component() );
+$(document).ready(function () {
+	$('#triangleSideLengths').submit(function (event) {
+		event.preventDefault();
+		var sideOneInput = parseInt($("#sideA").val());
+		var sideTwoInput = parseInt($("#sideB").val());
+		var sideThreeInput = parseInt($("#sideC").val());
 
-/*
-$("#triangleSideLengths").submit(function(event) {
-var sideOneInput = parseInt($("#sideOne").val());
-var sideTwoInput = parseInt($("#sideTwo").val());
-var sideTwoInput = parseInt($("#sideTwo").val());
-var sideThreeInput = parseInt($("#sideThree").val());
-
-event.preventDefault();
-if (isNotTriangle(sideOneInput, sideTwoInput, sideThreeInput)) {
-$("#formOutput").text("NOT a triangle: The sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side.");
-} else if (isEquilateral(sideOneInput, sideTwoInput, sideThreeInput)) {
-$("#formOutput").text("Equilateral: All sides are equal.");
-} else if (isIsosceles(sideOneInput, sideTwoInput, sideThreeInput)) {
-$("#formOutput").text("Isosceles: Exactly 2 sides are equal.");
-} else if (isScalene(sideOneInput, sideTwoInput, sideThreeInput)) {
-$("#formOutput").text("Scalene: No sides are equal.");
-} else {
-console.log("else");
-}
+		event.preventDefault();
+		if(Triangle.isNotTriangle(sideOneInput, sideTwoInput, sideThreeInput)) {
+			$("#formOutput").text("NOT a triangle: The sum of the lengths of any two sides of a triangle is less than or equal to the length of the third side.");
+		} else if(Triangle.isEquilateral(sideOneInput, sideTwoInput, sideThreeInput)) {
+			$("#formOutput").text("Equilateral: All sides are equal.");
+		} else if(Triangle.isIsosceles(sideOneInput, sideTwoInput, sideThreeInput)) {
+			$("#formOutput").text("Isosceles: Exactly 2 sides are equal.");
+		} else if(Triangle.isScalene(sideOneInput, sideTwoInput, sideThreeInput)) {
+			$("#formOutput").text("Scalene: No sides are equal.");
+		} else {
+			console.log("else");
+		}
+	});
 });
-});
-*/
--- webpack.common.js
+++ webpack.common.js
@@ -1,20 +1,34 @@
-const path = require( 'path' );
-const CleanWebpackPlugin = require( 'clean-webpack-plugin' );
-const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require("webpack");
 
 module.exports = {
 	entry: {
 		index: './app/src/index.js'
 	},
 	plugins: [
-    new CleanWebpackPlugin( [ 'build' ] ),
-    new HtmlWebpackPlugin( {
+		new webpack.ProvidePlugin({
+			$: 'jquery',
+			jQuery: 'jquery'
+		}),
+    new CleanWebpackPlugin(['build']),
+    new HtmlWebpackPlugin({
 			template: 'app/templates/index.html',
 			title: 'Production'
-		} )
+		})
   ],
 	output: {
 		filename: '[name].bundle.js',
-		path: path.resolve( __dirname, 'build' )
+		path: path.resolve(__dirname, 'build')
+	},
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: [
+				'style-loader',
+				'css-loader'
+			]
+		}]
 	}
 };
